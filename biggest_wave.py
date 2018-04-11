d59feed62d2a9ab4086276486ce56bb73deb8600
Daniella <daniella4422@gmail.com>
2018-04-10 17:19:50 -0700

    can make named directory

--- biggest_wave.py
+++ biggest_wave.py
@@ -10,4 +10,33 @@
 #                                                                              #
 # **************************************************************************** #
 
+#!usr/bin/python3
+import sys
+import os
 
+# def find_fastest_wave():
+#
+# def find_largest_wave():
+#
+# def make_waves():
+#
+# def make_lists():
+#
+# def make_csv():
+#
+# def split_file(filename):
+#
+def mkdir(filename):
+    path = os.getcwd()
+    path += "/" + filename + "_parsed"
+    os.mkdir(path)
+
+def main():
+    if (len(sys.argv) > 1):
+        filename = sys.argv[1]
+    else:
+       print ("enter the name of the earthquake data file that you wish to parse")
+       sys.exit(0)
+    mkdir(filename)
+
+main()
