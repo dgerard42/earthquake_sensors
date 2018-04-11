49bc9e55cd694ae74aaa277ec41fa700da413f4b
Daniella <daniella4422@gmail.com>
2018-04-10 21:51:32 -0700

    it makes nice directories and has stupid user input catches

--- biggest_wave.py
+++ biggest_wave.py
@@ -24,19 +24,27 @@ import os
 #
 # def make_csv():
 #
-# def split_file(filename):
-#
 def mkdir(filename):
     path = os.getcwd()
     path += "/" + filename + "_parsed"
     os.mkdir(path)
+    print ("directory '"+path+"' was created")
+
+def split_file(filename):
+    try:
+        eq_file = open(filename)
+    except:
+        return 0
+    mkdir(filename)
 
 def main():
     if (len(sys.argv) > 1):
         filename = sys.argv[1]
+        if (split_file(filename) == 0):
+           print ("file '"+filename+"' not found")
+           return 0
     else:
        print ("enter the name of the earthquake data file that you wish to parse")
-       sys.exit(0)
-    mkdir(filename)
+       return 0
 
 main()
