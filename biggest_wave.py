b1e08ea1497237b6937f2cdba2295b5b33e90fda
Daniella <daniella4422@gmail.com>
2018-04-14 12:24:42 -0700

    doot

--- biggest_wave.py
+++ biggest_wave.py
@@ -15,10 +15,22 @@ import sys
 import os
 
 # def find_fastest_wave(waves):
-    #iterate through the waves
+
 # def find_largest_wave(waves):
 
-# def make_waves(): #make a data frame of waves with wave size, wave time, and wave acceleration
+def make_waves(displacements): #make a 3d array of waves with wave size, wave time, and wave acceleration
+    new_wave = -1
+    time = 0.0
+    size = 0.0
+    sign = (displacements[0] < 0) ? false : true #true is positive, false is negative
+    for i in range(len(displacements)):
+        time += 0.01
+        size += displacements[i]
+        if (new_wave == 2)
+            
+        elif (displacements[i] < 0 && sign == true || displacements[i] > 0 && sign == false)
+            sign = (sign == true) ? false : true
+            new_wave += 1
 
 def split_by_direction(eq_file): #make 3 subarrays of the displacements for each direction
     displacements = [[], [], []]
@@ -49,7 +61,9 @@ def main():
            print ("file '"+filename+"' not found")
            return 0
         displacements = split_by_direction(eq_file)
-        print (displacements)
+        waves = [[],[],[]]
+        for direction in displacements
+            waves[direction] = make_waves[displacements[direction]]
     else:
        print ("enter the name of the earthquake data file that you wish to parse")
        return 0
